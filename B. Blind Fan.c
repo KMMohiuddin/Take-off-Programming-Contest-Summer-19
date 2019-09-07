cff7cd9712913dfa5baa41bf10d87228909b1183
Mohiuddin <52062238+Mohiuddin-diu@users.noreply.github.com>
2019-09-07 21:47:07 +0600

    Create B. Blind Fan.c

---
+++ B. Blind Fan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,s;
+    scanf("%d%d",&a,&s);
+    if(a==s)
+    {
+        printf("It is the final match\n");
+    }
+    else{printf("It is not the final match\n");}
+
+    return 0;
+}
